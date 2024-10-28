b7fff2ccf5eb547948701f4bbf6dfb89af61cfc2
as1248 <88760762+as1248@users.noreply.github.com>
2024-10-28 09:39:13 +0900

    Create MainPage
    메인 페이지 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HeaderComponent from "src/components/common/HeaderComponent";
-import FooterComponent from "src/components/common/FooterComponent";
+import MainPage from "./routes/MainPage";
 import MemberList from "./components/member/MemberList";
 import MemberSignUp from "./components/member/MemberSignUp";
 
 const Router = () => {
   return (
     <BrowserRouter>
-        <HeaderComponent />
         <Routes>
+          <Route path="/" element={<MainPage />} />
           {/* // http://localhost:3000/admin/memberList */}
           <Route path="/admin/memberList" element={<MemberList />}></Route>
           {/* // http://localhost:3000/signup */}
           <Route path="/signup" element={<MemberSignUp />}></Route>
         </Routes>
-        <FooterComponent />
       </BrowserRouter>
   )
 }
--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,9 +1,89 @@
+import styled from "styled-components";
+import { FaRegHeart } from "react-icons/fa";
+import HeaderComponent from "../components/common/HeaderComponent";
+
 const MainPage = () => {
   return (
     <>
-      <div>메인 페이지</div>
+      <HeaderComponent />
+      <Container>
+        <BoardCard>
+          <BoardImg src="#" alt="이미지1" as="div" />
+          <BoardCardBottom>
+            <div>제목1</div>
+            <BoardLike>
+              <FaRegHeart />
+              <div>0</div>
+            </BoardLike>
+          </BoardCardBottom>
+        </BoardCard>
+        <BoardCard>
+          <BoardImg src="#" alt="이미지2" as="div" />
+          <BoardCardBottom>
+            <div>제목2</div>
+            <BoardLike>
+              <FaRegHeart />
+              <div>0</div>
+            </BoardLike>
+          </BoardCardBottom>
+        </BoardCard>
+        <BoardCard>
+          <BoardImg src="#" alt="이미지3" as="div" />
+          <BoardCardBottom>
+            <div>제목3</div>
+            <BoardLike>
+              <FaRegHeart />
+              <div>0</div>
+            </BoardLike>
+          </BoardCardBottom>
+        </BoardCard>
+      </Container>
+      <Container>
+        <ProductCard>1</ProductCard>
+        <ProductCard>2</ProductCard>
+        <ProductCard>3</ProductCard>
+        <ProductCard>4</ProductCard>
+        <ProductCard>5</ProductCard>
+        <ProductCard>6</ProductCard>
+      </Container>
     </>
   )
 };
 
 export default MainPage;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 5rem 0 10rem;
+`;
+
+const BoardCard = styled.div`
+  width: 300px;
+  height: 300px;
+  border: 1px solid black;
+  border-radius: 1rem;
+`;
+
+const BoardImg = styled.img`
+  width: 300px;
+  height: 250px;
+`;
+
+const BoardCardBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`;
+
+const BoardLike = styled.div`
+  width: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ProductCard = styled(BoardCard)`
+  width: 200px;
+  height: 200px;
+`;
