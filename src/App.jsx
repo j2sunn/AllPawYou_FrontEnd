c74ed9f36d950a79a3e36094ef4cd183e008150e
as1248 <88760762+as1248@users.noreply.github.com>
2024-10-28 09:40:42 +0900

    Theme 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import "./App.css";
 import Router from "./Router";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#527853'
+      }
+    }
+  });
+
   return (
     <>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
     </>
   );
 }
