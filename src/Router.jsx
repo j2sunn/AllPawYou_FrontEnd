4c3d15f54e5256c35ab4d9715b5d3520a565dbb6
as1248 <88760762+as1248@users.noreply.github.com>
2024-10-28 09:39:38 +0900

    Create NotFoundPage
    404 에러 페이지 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./routes/MainPage";
 import MemberList from "./components/member/MemberList";
 import MemberSignUp from "./components/member/MemberSignUp";
+import NotFound from "./routes/NotFound";
 
 const Router = () => {
   return (
@@ -13,6 +14,8 @@ const Router = () => {
           <Route path="/admin/memberList" element={<MemberList />}></Route>
           {/* // http://localhost:3000/signup */}
           <Route path="/signup" element={<MemberSignUp />}></Route>
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
   )
