64c2b30434a99cb5b9ec5895fbc8265fdb42be65
Lee Ji Seon <140031241+j2sunn@users.noreply.github.com>
2024-10-29 16:23:34 +0900

    Reapply "241029 프론트 수정 작업"
    This reverts commit 1474ed705d33fbe2c7d58607428b5973bdc91622.

--- src/components/common/FooterComponent.jsx
+++ src/components/common/FooterComponent.jsx
@@ -39,16 +39,17 @@ const FooterComponent = () => {
           <Content>
             <Board>
               <p><StyledLink href="/signup">공지사항 </StyledLink> | <StyledLink href=""> FAQ</StyledLink></p>
+              <span>주식회사 올포유 | 대표 홍길동</span><br/>
               <p>
                 Copyright 2024. AllPawYou All rights reserved.
               </p>
             </Board>
             <Info>
-              <span>주식회사 올포유 | 대표 홍길동</span><br />
-              <span>
+              
+              {/* <span>
                 <StyledLink href="https://github.com/j2sunn/AllPawYou_FrontEnd">https://github.com/j2sunn/AllPawYou_FrontEnd</StyledLink><br />
                 <StyledLink href="https://github.com/j2sunn/AllPawYou_BackEnd">https://github.com/j2sunn/AllPawYou_BackEnd</StyledLink>
-              </span>
+              </span> */}
             </Info>
           </Content>
         </FooterContainer>
