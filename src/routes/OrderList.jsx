7b898c916c2572e2e9e41931e6570b5917a6c891
as1248 <88760762+as1248@users.noreply.github.com>
2024-11-04 15:35:17 +0900

    Update OrderList
    주문 목록 페이지 UI 수정

--- src/routes/OrderList.jsx
+++ src/routes/OrderList.jsx
@@ -13,48 +13,65 @@ const OrderList = () => {
           <SideBar>사이드바</SideBar>
           <Orders>
             <Order>
-              <OrderInfo>
-                <Product>
+              <OrderHeader>
+                <OrderTitle>주문 1</OrderTitle>
+                <div>
+                  <Button variant="outlined">주문 상세</Button>
+                  <Button variant="outlined" sx={{marginLeft: '1rem'}}>주문 취소</Button>
+                </div>
+              </OrderHeader>
+              <Product>
+                <OrderInfo>
                   <ProductImg as="div" />
                   <Detail>
                     <div>제품명</div>
                     <div>설명</div>
                     <div>가격</div>
                   </Detail>
-                </Product>
-                <Product>
+                </OrderInfo>
+                <Buttons>
+                  <Button variant="outlined">후기 작성하기</Button>
+                  <Button variant="outlined" color="error">후기 삭제하기</Button>
+                </Buttons>
+              </Product>
+              <Product>
+                <OrderInfo>
                   <ProductImg as="div" />
                   <Detail>
                     <div>제품명</div>
                     <div>설명</div>
                     <div>가격</div>
                   </Detail>
-                </Product>
-              </OrderInfo>
-              <Buttons>
-                <Button variant="outlined">주문 상세</Button>
-                <Button variant="outlined">주문 취소</Button>
-                <Button variant="outlined">후기 작성하기</Button>
-                <Button variant="outlined" color="error">후기 삭제하기</Button>
-              </Buttons>
+                </OrderInfo>
+                <Buttons>
+                  <Button variant="outlined">후기 작성하기</Button>
+                  <Button variant="outlined" color="error">후기 삭제하기</Button>
+                </Buttons>
+              </Product>
             </Order>
+
             <Order>
-              <OrderInfo>
-                <Product>
+              <OrderHeader>
+                <OrderTitle>주문 2</OrderTitle>
+                <div>
+                  <Button variant="outlined">주문 상세</Button>
+                  <Button variant="outlined" sx={{marginLeft: '1rem'}}>주문 취소</Button>
+                </div>
+              </OrderHeader>
+              <Product>
+                <OrderInfo>
                   <ProductImg as="div" />
                   <Detail>
                     <div>제품명</div>
                     <div>설명</div>
                     <div>가격</div>
                   </Detail>
-                </Product>
-              </OrderInfo>
-              <Buttons>
-                <Button variant="outlined">주문 상세</Button>
-                <Button variant="outlined">주문 취소</Button>
-                <Button variant="outlined">후기 작성하기</Button>
-                <Button variant="outlined" color="error">후기 삭제하기</Button>
-              </Buttons>
+                </OrderInfo>
+                <Buttons>
+                  <Button variant="outlined">후기 작성하기</Button>
+                  <Button variant="outlined" color="error">후기 삭제하기</Button>
+                </Buttons>
+              </Product>
             </Order>
           </Orders>
         </Content>
@@ -93,28 +110,41 @@ const Orders = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin: 0 2rem;
 `;
 
 const Order = styled.div`
-  display: flex;
+  width: 100%;
   border: 3px solid #EEC759;
   border-radius: 10px;
-  width: 80%;
+  margin-bottom: 2rem;
+`;
+
+const OrderHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem 2rem 0;
+`;
+
+const OrderTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const Product = styled.div`
+  display: flex;
+  border: 1px solid #EEC759;
+  border-radius: 10px;
+  width: 90%;
   margin: 2rem;
   padding: 1rem;
 `;
 
 const OrderInfo = styled.div`
   display: flex;
-  flex-direction: column;
   padding: 1rem;
   width: 75%;
   border-right: 1px solid black;
-`;
-
-const Product = styled.div`
-  display: flex;
-  margin-bottom: 1rem;
 `;
 
 const ProductImg = styled.img`
@@ -136,6 +166,8 @@ const Buttons = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
   margin-left: 2rem;
   button {
     margin: 0.5rem;
