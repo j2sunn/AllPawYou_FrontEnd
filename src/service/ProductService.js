4f8a759ede28fa4a47acfb2c0f6887687658dd2a
Lee Ji Seon <140031241+j2sunn@users.noreply.github.com>
2024-11-07 01:41:56 +0900

    241107 상품 상세조회

--- src/service/ProductService.js
+++ src/service/ProductService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 // import { AuthApi } from "./Auth";
 
 const REST_API_BASE_URL =  'http://localhost:8081/api/shopping';
+const token = localStorage.getItem('accessToken'); 
 
 export const addProduct = (formData)=>axios.post(REST_API_BASE_URL + "/addProduct",formData,{
     headers: {
@@ -12,5 +13,13 @@ export const addProduct = (formData)=>axios.post(REST_API_BASE_URL + "/addProduc
 });
 
 export const listProducts = () => axios.get(REST_API_BASE_URL);
+
+// export const getProductByProductId = (id) => axios.get(`${REST_API_BASE_URL}/${id}`);
+export const getProductByProductId = (id) => axios.get(REST_API_BASE_URL + "/"+ id, {
+    headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': token ? `Bearer ${token}` : '' // 인증이 필요하면 헤더에 JWT 토큰 포함
+    }
+});
 
 
