a4d155793dc58bbcccad5c63a941f438afc2908c
as1248 <88760762+as1248@users.noreply.github.com>
2024-11-07 18:15:40 +0900

    유저 사이드바 수정

--- src/components/common/MypageSideBar.jsx
+++ src/components/common/MypageSideBar.jsx
@@ -1,15 +1,10 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { SimpleTreeView, TreeItem } from "@mui/x-tree-view";
-import { Button } from "@mui/material";
 
 const MypageSideBar = () => {
   const navigate = useNavigate();
 
-  const goMyBoard = () => {
-    console.log("Navigating to /review/myReview"); // 확인 로그
-    navigate(`/`);
-  };
 
   return (
     <>
@@ -20,10 +15,7 @@ const MypageSideBar = () => {
           <TreeItem itemId="updateProfile" label="프로필 수정" sx={{ marginBottom: "2rem", "& .MuiTreeItem-label": { fontSize: "1.2rem" } }} />
           <TreeItem itemId="order" label="구매 내역" sx={{ marginBottom: "2rem", "& .MuiTreeItem-label": { fontSize: "1.2rem" } }} />
           <TreeItem itemId="myData" label="내 활동" sx={{ marginBottom: "2rem", "& .MuiTreeItem-label": { fontSize: "1.2rem" } }}>
-            <TreeItem itemId="myBoard" label="내 글 관리" />
-            <Button type="button" onClick={goMyBoard}>
-              내 글 관리로 이동
-            </Button>
+            <TreeItem itemId="myBoard" label="내 글 관리" onClick={()=>navigate(`/review/myReview`)}/>
             <TreeItem itemId="myReview" label="내 후기 관리" />
           </TreeItem>
         </SimpleTreeView>
