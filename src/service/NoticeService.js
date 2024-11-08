d20f45b16407813606ddf83807babc0330ab2dc5
sunfull727 <150773386+sunfull727@users.noreply.github.com>
2024-11-08 09:04:33 +0900

    11/08 공지사항 게시판 작업

--- src/service/NoticeService.js
+++ src/service/NoticeService.js
@@ -6,10 +6,10 @@ const REST_API_URL = `${REST_API_BASE_URL}/notice`;
 
 export const listNotices = () => axios.get(`${REST_API_URL}`);
 
-export const createNotice = (member) => axios.post(REST_API_URL, member);
+export const createNotice = (notice) => axios.post(REST_API_URL, notice);
 
 export const getNotice = (noticeNo) => axios.get(REST_API_URL + "/" + noticeNo);
 
-export const updateNotice = (noticeNo, member) => axios.put(REST_API_URL + "/update/" + noticeNo, member);
+export const updateNotice = (noticeNo, notice) => axios.put(REST_API_URL + "/update/" + noticeNo, notice);
 
 export const deleteNotice = (noticeNo) => axios.delete(REST_API_URL + "/delete/" + noticeNo);
