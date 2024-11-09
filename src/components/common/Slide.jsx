9d4f3be3632cb847220a749754593472dcfd893d
as1248 <88760762+as1248@users.noreply.github.com>
2024-11-09 17:00:31 +0900

    메인페이지 임시 배너

--- src/components/common/Slide.jsx
+++ src/components/common/Slide.jsx
@@ -10,11 +10,11 @@ export default function SimpleSlider() {
   const slideRef = useRef(null);
 
   function SamplePrevArrow() {
-    return <IoIosArrowBack size={32} onClick={() => slideRef.current.slickPrev()} style={{display: 'block', color: 'black', width: '30px'}}/>;
+    return <IoIosArrowBack size={64} onClick={() => slideRef.current.slickPrev()} style={{display: 'block', color: 'white', position: 'sticky', left: '10px', zIndex: 10}} />;
   }
 
   function SampleNextArrow() {
-    return <IoIosArrowForward size={32} onClick={() => slideRef.current.slickNext()} style={{display: 'block', }}/>;
+    return <IoIosArrowForward size={64} onClick={() => slideRef.current.slickNext()} style={{display: 'block', color: 'white', position: 'sticky', right: '10px',  zIndex: 10}} />;
   }
   
   const settings = {
@@ -23,81 +23,46 @@ export default function SimpleSlider() {
     speed: 500, // 속도
     autoplay: true, // 자동 재생
     autoplaySpeed: 3000, // 자동 재생 속도
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     arrow: false,
   };
 
   return (
     <SliderWrapper>
-    <SamplePrevArrow />
-    <StyledSlider {...settings} ref={slideRef}>
-      <div>
-        <ProductCard>
-          <ProductImg src="/src/assets/mainImage/shopping1.png" alt="이미지1" />
-          <ProductCardBottom>
-            <div>go 그레인프리 건식사료</div>
-            <div>75,150원</div>
-          </ProductCardBottom>
-        </ProductCard>
-      </div>
+      <SamplePrevArrow />
+      <StyledSlider {...settings} ref={slideRef}>
+        <div>
+          <ProductCard>
+            <ProductImg src="/src/assets/mainImage/main1.jpg"/>
+          </ProductCard>
+        </div>
 
-      <div>
-        <ProductCard>
-          <ProductImg src="/src/assets/mainImage/shopping2.png" alt="이미지1" />
-          <ProductCardBottom>
-            <div>강아지 배변패드 100매</div>
-            <div>12,900원</div>
-          </ProductCardBottom>
-        </ProductCard>
-      </div>
+        <div>
+          <ProductCard>
+            <ProductImg src="/src/assets/mainImage/main2.jpg"/>
+          </ProductCard>
+        </div>
 
-      <div>
-        <ProductCard>
-          <ProductImg src="/src/assets/mainImage/shopping3.png" alt="이미지1" />
-          <ProductCardBottom>
-            <div>반려동물 마약 방석</div>
-            <div>9,800원</div>
-          </ProductCardBottom>
-        </ProductCard>
-      </div>
-
-      <div>
-        <ProductCard>
-          <ProductImg src="/src/assets/mainImage/shopping4.jpg" alt="이미지1" />
-          <ProductCardBottom>
-            <div>바잇미 강아지 하네스</div>
-            <div>25,160원</div>
-          </ProductCardBottom>
-        </ProductCard>
-      </div>
-
-      <div>
-        <ProductCard>
-          <ProductImg src="/src/assets/mainImage/shopping5.jpg" alt="이미지1" />
-          <ProductCardBottom>
-            <div>반려동물 리드줄</div>
-            <div>13,900원</div>
-          </ProductCardBottom>
-        </ProductCard>
-      </div>
-
-    </StyledSlider>
-    <SampleNextArrow />
+        <div>
+          <ProductCard>
+            <ProductImg src="/src/assets/mainImage/main3.jpg"/>
+          </ProductCard>
+        </div>
+      </StyledSlider>
+      <SampleNextArrow />
     </SliderWrapper>
   );
 }
 const SliderWrapper = styled.div`
   display: flex;
+  width: 110%;
   align-items: center;
-  justify-content: space-evenly;
-  margin: 3rem 0 5rem;
+
 `;
 
 const StyledSlider = styled(Slider)`
-display: flex;
-  width: 90%;
-  position: relative;
+  width: 93%;
   .slick-prev::before,
   .slick-next::before {
     opacity: 0;
@@ -107,22 +72,13 @@ display: flex;
 
 const ProductCard = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
-  width: 200px;
-  height: 220px;
-  margin-left: 20%;
+  width: 100%;
+  height: 800px;
 `;
 
 const ProductImg = styled.img`
-  width: 200px;
-  height: 150px;
+  width: 100%;
+  height: 800px;
   box-shadow: 0px 0px 5px #444;
-`;
-
-const ProductCardBottom = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 1rem;
-  flex-direction: column;
 `;
