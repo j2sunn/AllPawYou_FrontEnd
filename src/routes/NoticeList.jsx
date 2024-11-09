b10148bddb2c59e3bda3da3ea6a560a3f144f9bc
sunfull727 <150773386+sunfull727@users.noreply.github.com>
2024-11-09 16:50:31 +0900

    11/09 공지사항 작업

--- src/routes/NoticeList.jsx
+++ src/routes/NoticeList.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { deleteNotice, listNotices } from "../service/NoticeService";
 //import HeaderComponent from "../components/common/HeaderComponent";
@@ -5,28 +6,31 @@ import { SimpleTreeView } from "@mui/x-tree-view/SimpleTreeView";
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
 import styled from "styled-components";
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import AdminSideBar from "../components/common/AdminSideBar";
 //import FooterComponent from "../components/common/FooterComponent";
 
 const ListNoticeComponent = () => {
   const [notices, setNotices] = useState([
-    {
-      noticeTitle: "a",
-      noticeDate: "2024-11-02",
-    },
-    {
-      noticeTitle: "b",
-      noticeDate: "2024-11-02",
-    },
-    {
-      noticeTitle: "c",
-      noticeDate: "2024-11-02",
-    },
-    {
-      noticeTitle: "d",
-      noticeDate: "2024-11-02",
-    },
+    // {
+    //   noticeTitle: "a",
+    //   noticeDate: "2024-11-02",
+    // },
+    // {
+    //   noticeTitle: "b",
+    //   noticeDate: "2024-11-02",
+    // },
+    // {
+    //   noticeTitle: "c",
+    //   noticeDate: "2024-11-02",
+    // },
+    // {
+    //   noticeTitle: "d",
+    //   noticeDate: "2024-11-02",
+    // },
   ]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     getAllNotices();
@@ -43,17 +47,34 @@ const ListNoticeComponent = () => {
       });
   }
 
+  
+  const goAddNotice = () => {
+    navigate(`/admin/noticeWrite`);
+  }
+
   function removeNotice(noticeNo) {
     console.log(noticeNo);
 
-    deleteNotice(noticeNo)
-      .then((response) => {
-        console.log(response);
-        getAllNotices();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if(confirm("정말 삭제하시겠습니까?")){
+        axios.delete("http://localhost:8081/api/notice/delete/"+noticeNo)
+        .then(resp=>{
+            console.log("결과느으으은"+resp.data);
+              // alert("삭제되었습니다.");
+              navigate("/admin/noticeList");
+            
+        })
+    }
+
+
+
+    // deleteNotice(noticeNo)
+    //   .then((response) => {
+    //     console.log(response);
+    //     getAllNotices();
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   }
 
   return (
@@ -62,46 +83,39 @@ const ListNoticeComponent = () => {
         <AdminSideBar />
         <Content>
           <Title>공지사항 관리</Title>
-          <br/>
-          <Button variant="outlined" color="error" sx={{width: '20%'}}>
-            새 공지사항
-          </Button>
           <TableContainer component={Paper} sx={{ width: "90%", marginTop: "3rem" }}>
             <Table>
               <TableHead sx={{ backgroundColor: "#EEC759" }}>
                 <TableRow>
-                  <TableCell align="center" sx={{width: '14%'}}>번호</TableCell>
-                  <TableCell align="center" sx={{width: '30%'}}>제목</TableCell>
-                  <TableCell align="center" sx={{width: '30%'}}>등록일</TableCell>
-                  <TableCell align="center" sx={{ width: "8%" }}>
-                    수정
-                  </TableCell>
-                  <TableCell align="center" sx={{ width: "8%" }}>
-                    삭제
+                  <TableCell align="center" sx={{ width: "8rem" }}>번호</TableCell>
+                  <TableCell align="center" sx={{ width: "20rem" }}>제목</TableCell>
+                  <TableCell align="center" sx={{ width: "10rem" }}>등록일</TableCell>
+                  <TableCell align="center" sx={{ width: "15rem" }}>
+                    수정/삭제
                   </TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {notices.map((item, index) => (
                   <TableRow key={item.noticeTitle}>
-                    <TableCell align="center" sx={{width: '14%'}}>{index + 1}</TableCell>
-                    <TableCell align="center" sx={{width: '30%'}}>{item.noticeTitle}</TableCell>
-                    <TableCell align="center" sx={{width: '30%'}}>{item.noticeDate}</TableCell>
-                    <TableCell align="center" sx={{ width: "8%" }}>
-                      <Button variant="outlined" color="error">
-                        수정
-                      </Button>
-                    </TableCell>
-                    <TableCell align="center" sx={{ width: "8%" }} onClick={() => removeNotice(item.noticeNo)}>
-                      <Button variant="outlined" color="error">
-                        삭제
-                      </Button>
-                    </TableCell>
+                    <TableCell align="center">{index + 1}</TableCell>
+                    <TableCell align="center">{item.noticeTitle}</TableCell>
+                    <TableCell align="center">{item.noticeDate}</TableCell>
+                        <TableCell align="center" sx={{ width: "10rem" }} >
+                            <Button variant="contained"
+                                sx={{ marginRight: '10px' }}>수정</Button>
+                            <Button variant="outlined" onClick={() => removeNotice(item.noticeNo)}>삭제</Button>
+                        </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </TableContainer>
+          
+          <AddProductButton>
+                <Button variant="contained" sx={{ marginTop : '25px'}}
+                onClick={() => goAddNotice()}>공지사항 등록</Button>
+          </AddProductButton>
           <Pages>1 2 3 4 5 6</Pages>
         </Content>
       </Container>
@@ -142,3 +156,8 @@ const Pages = styled.div`
   justify-content: center;
 `;
 
+const AddProductButton = styled.div`
+     display: flex;
+  justify-content: flex-end;
+   width: 90%; 
+`;
