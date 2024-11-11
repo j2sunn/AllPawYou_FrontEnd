7932d6fd1f0bfc8b998122461631ee5497b0412f
as1248 <88760762+as1248@users.noreply.github.com>
2024-11-11 15:11:57 +0900

    공지사항 수정 페이지 기능 수정
    잘못된 url 접근시 예외처리

--- src/routes/Admin/components/aUpdateNotice.jsx
+++ src/routes/Admin/components/aUpdateNotice.jsx
@@ -3,11 +3,12 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { updateNotice } from "../../../service/NoticeService";
 import styled from "styled-components";
 import Swal from "sweetalert2";
+import { Button } from "@mui/material";
 const NoticeUpdate = () => {
     
     const  {state}  = useLocation();
     
-    const [noticeNo, setNoticeNo] = useState(state.noticeNo);
+    const [noticeNo, setNoticeNo] = useState(state?.noticeNo);
     const [noticeData, setNoticeData] = useState(null);
     const [noticeTitle, setNoticeTitle] = useState("");
     const [noticeContent, setNoticeContent] = useState(""); //이거는 한 자라도 작성했는지 체크용
@@ -21,9 +22,9 @@ const NoticeUpdate = () => {
     console.log("수정할 noticeNo : " + noticeNo);
     useEffect(()=>{
         console.log(state);
-        setNoticeTitle(state.noticeTitle);
-        setNoticeContent(state.noticeContent);
-        setResult(state.noticeContent.replace(/\n/g, '<e>').replace(/ /g, '<s>'));
+        setNoticeTitle(state?.noticeTitle);
+        setNoticeContent(state?.noticeContent);
+        setResult(state?.noticeContent.replace(/\n/g, '<e>').replace(/ /g, '<s>'));
     },[]);
 
     // 제목 수정 핸들러
@@ -97,7 +98,7 @@ const handleTitleChange = (e) => {
             {state ? (
                     <>
                     <Title>공지사항 수정</Title>
-                    <One>
+                    <Content>
                         <h5>제목<span>*</span></h5>
                         <TitleInput onChange={handleTitleChange} 
                                 value={noticeTitle}/>
@@ -108,13 +109,17 @@ const handleTitleChange = (e) => {
                                 value={noticeContent}/>
                         
                         <Error>{error.content}</Error>
-                    </One>
-                    <Btn onClick={cancel}>취소</Btn>
-                    <EndBtn onClick={doSubmit}>수정</EndBtn>
+                    </Content>
+                    <Buttons>
+                        <Button variant="outlined" color="error" onClick={cancel} sx={{width: '5rem', marginRight: '2rem'}}>취소</Button>
+                        <Button variant="contained" onClick={doSubmit} sx={{width: '5rem'}}>수정</Button>
+                    </Buttons>
                 </>
             ) : (
-                <>
-                </>
+                <NoData>
+                    존재하지 않는 공지사항입니다.
+                    <Button variant="contained" onClick={()=>navigator("/admin/noticeList")} sx={{fontSize: '1.5rem', marginTop: '2rem'}}>공지사항 목록으로</Button>
+                </NoData>
             )}
             
         </>
@@ -123,18 +128,20 @@ const handleTitleChange = (e) => {
 export default NoticeUpdate;
 
 const Title = styled.div`
-
-font-size: 2rem;
-padding-bottom: 3rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding-top: 3rem;
+  margin-left: 3rem;
   width: 90%;
-  border-bottom: 3px solid #c4e1f6;
 `;
 
-const One = styled.div`
-    width: 80%;
+const Content = styled.div`
+    width: 90%;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    margin-left: 3rem;
+    margin-bottom: 3rem;
     h5{
         margin-top: 30px;
         
@@ -177,26 +184,22 @@ const ContentTextarea = styled.textarea`
     }
 `;
 
-const Btn = styled.button`
-    background-color: white;
-    border : 3px solid #EEC759;
-    margin : 30px 0;
-    border-radius: 15px;
-    padding : 5px;
-    width : 100px;
-    font-weight: bold;
-    border-radius: 20px;
-    text-align: center;
-    padding : 7px;
-`;
-
-const EndBtn = styled(Btn)`
-    background-color: #EEC759;
-    &:hover{
-        background-color: white;
-    }
+const Buttons = styled.div`
+    display: flex;
+    justify-content: end;
+    margin-right: 10rem;
 `;
 
 const Error = styled.div`
     color: red;
+`;
+
+const NoData = styled.div`
+    font-size: 2rem;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
