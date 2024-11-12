51719a253ecc56cce2eb8ab3612f482751594274
as1248 <88760762+as1248@users.noreply.github.com>
2024-11-12 09:31:21 +0900

    메인페이지 인기글 리스트 수정

--- src/components/common/ImageList.jsx
+++ src/components/common/ImageList.jsx
@@ -22,17 +22,21 @@ const BoardImageList = () => {
       no=Number(localStorage.getItem('no'));
   }
     axios.get("http://localhost:8081/board/mainTop")
-    .then(resp=>{
-      resp.data.map((item)=>{
-        // if(localStorage.getItem("no"));
-        
-        //item으로 하나씩 꺼내서 
-        //객체 만들어서 arr에 넣기
-        console.log(item.boardNo);
-        let obj = {img:'http://localhost:8081/images/board/'+item.imgRename, title:item.boardNo, rows:2, cols:2};
-        arr.push(obj);
-      })
-        setItemData(arr);
+    .then(response=>{
+      const arr = [];
+      arr.push({...response.data[0], rows: 2, cols: 2});
+      arr.push(response.data[1]);
+      arr.push(response.data[2]);
+      arr.push({...response.data[3], cols: 2});
+      arr.push({...response.data[4], cols: 2});
+      arr.push({...response.data[5], rows: 2, cols: 2});
+      arr.push(response.data[6]);
+      arr.push(response.data[7]);
+      arr.push({...response.data[8], rows: 2, cols: 2});
+      arr.push(response.data[9]);
+      arr.push(response.data[10]);
+      arr.push({...response.data[11], cols: 2});
+      setItemData(arr);
       })
 
 
@@ -61,64 +65,3 @@ const BoardImageList = () => {
 }
 
 export default BoardImageList;
-
-// const itemData = [
-//   {
-//     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-//     title: 'Breakfast',
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-//     title: 'Burger',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-//     title: 'Camera',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-//     title: 'Coffee',
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-//     title: 'Hats',
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-//     title: 'Honey',
-//     author: '@arwinneil',
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-//     title: 'Basketball',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-//     title: 'Fern',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-//     title: 'Mushrooms',
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//     cols: 2,
-//   },
-// ];
