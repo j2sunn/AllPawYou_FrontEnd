5a62b881f36a4c5c297d928c9cbb792150356075
VinoMatinee <hyuk6589@gmail.com>
2024-11-12 10:54:10 +0900

    2024.11.12_닉네임중복확인, 폼수정

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,8 +1,14 @@
 import { AuthApi } from "./AuthApi";
+
 // 유저 리스트
 export const listUsers = () => AuthApi.get(`/api/v2/users`);
+
 // 유저 삭제
 export const deleteUser = (no) => AuthApi.delete(`/api/v2/users/delete/${no}`);
+
 // 유저 권한 변경
-// http://localhost:8081/api/v2/users/updateRole?no=1&role=ROLE_SALER
 export const updateRole = (no, role) => AuthApi.put(`/api/v2/users/updateRole?no=${no}&role=${role}`);
+
+// 닉네임 중복체크
+// http://localhost:8081/api/v2/users/checkNickname?nickname=다붕
+export const checkNickname = (nickname) => AuthApi.get(`/api/v2/users/checkNickname?nickname=${nickname}`);
